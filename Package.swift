002f69740ebe1eb6fef07061b46de02d6a4ea4fe
Daniel Saidi <daniel.saidi@gmail.com>
2025-06-06 23:48:52 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -20,13 +20,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LicenseKit",
-            url: "https://github.com/LicenseKit/LicenseKit/releases/download/1.2.4_binary/LicenseKit.zip",
-            checksum: "389a58fc8148215a8f8fed06960aa24ddaba3a5b88e73093f60256ddf947cc1d"
+            url: "https://github.com/LicenseKit/LicenseKit/releases/download/1.4.1_binary/LicenseKit.zip",
+            checksum: "0f2d4bcebb2da6acc1a05692d174ba965be9c25b23cd71705274e30ac54cd5b3"
         ),
         .binaryTarget(
             name: "VietnameseInput",
-            url: "https://github.com/Kankoda/VietnameseInput/releases/download/0.0.5_binary/VietnameseInput.zip",
-            checksum: "794fdce20d26376a93d488fec52c06662a88e698970a6faf6794a5d6536a7a7d"
+            url: "https://github.com/Kankoda/VietnameseInput/releases/download/0.1.0_binary/VietnameseInput.zip",
+            checksum: "ce2c59a948b614b3d8beda390b0446e723115b1c28ff98ab085f21f6489875ec"
         )
     ]
 )
