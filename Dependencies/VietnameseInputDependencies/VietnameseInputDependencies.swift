b24c11d920a14289de897835370e48a60ee1ecff
Daniel Saidi <daniel.saidi@gmail.com>
2025-06-06 23:56:39 +0200

    Adjust package to test alternate dependency management

---
+++ Dependencies/VietnameseInputDependencies/VietnameseInputDependencies.swift
@@ -0,0 +1,12 @@
+//
+//  VietnameseInputDependencies.swift
+//  VietnameseInput
+//
+//  Created by Daniel Saidi on 2025-06-06.
+//
+
+import Foundation
+
+/// This class is only used to make the public package fetch
+/// dependencies for the binary target.
+public class VietnameseInputDependencies {}
--- Package.swift
+++ Package.swift
@@ -14,19 +14,22 @@ let package = Package(
     products: [
         .library(
             name: "VietnameseInput",
-            targets: ["VietnameseInput", "LicenseKit"]
+            targets: ["VietnameseInput", "VietnameseInputDependencies"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/LicenseKit/LicenseKit.git", .upToNextMajor(from: "1.4.0"))
+    ],
     targets: [
-        .binaryTarget(
-            name: "LicenseKit",
-            url: "https://github.com/LicenseKit/LicenseKit/releases/download/1.4.1_binary/LicenseKit.zip",
-            checksum: "0f2d4bcebb2da6acc1a05692d174ba965be9c25b23cd71705274e30ac54cd5b3"
-        ),
         .binaryTarget(
             name: "VietnameseInput",
             url: "https://github.com/Kankoda/VietnameseInput/releases/download/0.1.0_binary/VietnameseInput.zip",
             checksum: "ce2c59a948b614b3d8beda390b0446e723115b1c28ff98ab085f21f6489875ec"
+        ),
+        .target(
+            name: "VietnameseInputDependencies",
+            dependencies: ["LicenseKit"],
+            path: "Dependencies",
         )
     ]
 )
